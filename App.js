877fcaea9f981e615cd42ff12327853e1c711302
Lucas Santiago <lucas.santiago@radixeng.com.br>
2019-12-16 17:22:52 -0300

    Adição da searchbar

--- App.js
+++ App.js
@@ -3,11 +3,23 @@ import {Text, View, StyleSheet, TextInput} from 'react-native'; // 2
 
 // 3
 export default class App extends Component {
+  state = {
+    searchText: '',
+  }
+
   render() {
     return (
       // 4
-      <View style={styles.hello}>
-        <Text style={styles.text}>Hello, world!</Text>
+      <View style={styles.screen}>
+        <View>
+          <TextInput 
+            style={styles.input} 
+            placeholder={'Procure uma série'} 
+            onChangeText={(text) => this.setState({searchText: text})}/>
+        </View>
+        <View>
+          <Text>{this.state.searchText}</Text>
+        </View>
       </View>
     );
   }
@@ -15,13 +27,24 @@ export default class App extends Component {
 
 // 5
 const styles = StyleSheet.create({
-  hello: {
+  screen: {
     flex: 1,
-    justifyContent: 'center',
+    marginTop: 15,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
     alignItems: 'center',
   },
   text: {
     fontSize: 40,
     color: 'purple',
   },
+  input: {
+    borderWidth: 1,
+    borderRadius: 25,
+    borderColor: '#999999',
+    height: 50,
+    width: 270,
+    fontSize: 15,
+    padding: 15,
+  }
 });
